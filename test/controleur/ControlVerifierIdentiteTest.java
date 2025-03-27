a975c853c1a2cb7f7b99d0c4fc480d7faa56317c
wallymeqqori <wallymeqqori@gmail.com>
2025-03-27 17:45:44 +0100

    ControlAfficherVillageTest DONE, 36,3% coverage

--- test/controleur/ControlVerifierIdentiteTest.java
+++ test/controleur/ControlVerifierIdentiteTest.java
@@ -2,10 +2,33 @@ package controleur;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import personnages.Chef;
+import personnages.Druide;
+import villagegaulois.Village;
+
+
 
 class ControlVerifierIdentiteTest {
 
+	private ControlVerifierIdentite controlVerifierIdentite;
+	private Village village;
+	private Chef abraracourcix;
+	private Druide panoramix;
+	
+	@BeforeEach
+	public void initialiserSituation() {
+		village = new Village("le village des irréductibles", 10, 5);
+		abraracourcix = new Chef("Abraracourcix", 10, village);
+		village.setChef(abraracourcix);
+		controlVerifierIdentite = new ControlVerifierIdentite(village);
+	}	
+	
+	
+	
+	
 	@Test
 	void testControlVerifierIdentite() {
 		fail("Not yet implemented");
